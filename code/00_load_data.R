a7396a6dc915c7cdb62e13d2eb86c887ccdc889c
jhaas0 <80410223+jhaas0@users.noreply.github.com>
2025-12-07 17:06:48 -0500

    change file path for dataset

--- code/00_load_data.R
+++ code/00_load_data.R
@@ -2,6 +2,8 @@ library(tidyverse)
 
 setwd("C:/Users/Jacob/Documents/Wagner/RM/research_methods_final")
 
-migrate_2019 <- read_csv("data/15106182/2019.csv/2019.csv")
+migrate_2019 <- read_csv("data/2019.csv/2019.csv")
 
 head(migrate)
+
+
