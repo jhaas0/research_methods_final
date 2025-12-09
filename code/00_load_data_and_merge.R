f020ee796a6f3d38dedc9ebf4a28b7889ee11860
Leal <fl1351@law.nyu.edu>
2025-12-09 16:38:14 -0500

    MIH developments clean data

--- code/00_load_data_and_merge.R
+++ code/00_load_data_and_merge.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(tidycensus)
 library(tigris)
 library(sf)
+library(lubridate)
+library(dplyr)
 
 setwd("C://Users//fl1351//Desktop//RM Project Local//research_methods_final")
 
@@ -28,25 +30,55 @@ migrate_2019 <- migrate_2019 %>%
   )
 
 
-# Inclusionary housing data ####
-inc_housing_areas <- read_csv("data/inclusionaryhousing_areas.csv")
+# Inclusionary housing polygons ####
+#inc_housing_polygons<- read_csv("data/inclusionaryhousing_areas.csv")
 
-inclusionary_zoning_projects <- inchousing_areas$ProjectNam
+#inclusionary_zoning_projects <- inc+housing_polygons$ProjectNam
 
-print(inclusionary_zoning_projects)
+#print(inclusionary_zoning_projects)
 
 # Affordable housing Production by Building ####
-buildings_data <- read_csv("data/Affordable_Housing_Production_by_Building.csv")
+#buildings_data <- read_csv("data/Affordable_Housing_Production_by_Building.csv")
 
 #See if any projects are not represented in buildings data. None found directly
-setdiff(inclusionary_zoning_projects, buildings_data$`Project Name`)
+#setdiff(inclusionary_zoning_projects, buildings_data$`Project Name`)
 
-unique(buildings_data$`Project Name`)
+#unique(buildings_data$`Project Name`)
 
 # Affordable housing production by Project ####
-projects_data <- read_csv("data/Affordable_Housing_Production_by_Project.csv")
+#projects_data <- read_csv("data/Affordable_Housing_Production_by_Project.csv")
 
+#Inclusionary Zoning data! ####
 
+inclusionary_zoning_data <- read.csv("data/All HPD Inclusionary Housing Sites _Query result.csv")
+MIH_projects <- inclusionary_zoning_data %>%
+  filter(Label %in% c("MIH Site", "MIH Development"))
+#write.csv(mandatory_zoning_projects, "MIH_projects")
 
+# Only keep MIH developments, and collapse by project (one or multiple adjacent addresses)
+mih_developments_clean <- MIH_projects %>%
+  filter(Label == "MIH Development") %>%
+  # Arrange so:
+  # Completed projects come first (not needed), newest Transfer_Date first, objectid for stable ordering. Basically just making sure  row first represents project well
+  arrange(
+    Project_Name,
+    desc(Project_Status == "Completed"),
+    desc(!is.na(Transfer_Date)),
+    desc(Transfer_Date),
+    OBJECTID 
+  ) %>%
+  # Collapse by project
+  group_by(Project_Name) %>%
+  mutate(
+    Total_IH_Floor_Area = sum(Bldg_IH_Floor_Area, na.rm = TRUE)
+  ) %>%
+  slice_head(n = 1) %>% # Picking the first address to represent project
+  ungroup()
 
+#Drop extra variables:
+mih_developments_clean <- mih_developments_clean%>% 
+  select(-OBJECTID, -IH_Floor_Area_Transferred, -IH_Floor_Area_Balance,
+         -Comp_IH_Floor_Area)
+
+write.csv(mih_developments_clean, "mih_developments_clean.csv", row.names = FALSE)
 
