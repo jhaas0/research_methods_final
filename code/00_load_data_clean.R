d0038e3be86e811c800a02f8ebf39c99eeafff0e
jhaas0 <80410223+jhaas0@users.noreply.github.com>
2025-12-16 15:08:30 -0500

    Add event studies, join rather than rbind MIH buildings to block groups

--- code/00_load_data_clean.R
+++ code/00_load_data_clean.R
@@ -30,7 +30,8 @@ migrate_data <- read_csv(file_path)
   # Filter and clean
   migrate_nyc <- migrate_data %>% 
     clean_names() %>% 
-    mutate(origin = str_pad(origin, width = 12, side = "left", pad = "0")) %>% 
+    mutate(origin = str_pad(origin, width = 12, side = "left", pad = "0"), 
+           destination = str_pad(destination, width = 12, side = "left", pad = "0")) %>% 
     filter(str_detect(origin, "^(36005|36047|36061|36081|36085)") | 
              str_detect(destination, "^(36005|36047|36061|36081|36085)"))
   
